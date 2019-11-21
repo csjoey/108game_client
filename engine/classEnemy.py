f6a7c787fd7e2e0d8476320eedcc352f3134b6c9
Khaleeb <cdavis1530@gmail.com>
2019-11-21 12:59:43 -0500

    enemy movement

--- engine/classEnemy.py
+++ engine/classEnemy.py
@@ -1,12 +1,34 @@
+import random
+
+
 class Enemy:
 
     def __init__(self):
-        self.x = None
-        self.y = None
+        self.row = None
+        self.col = None
+        self.open_moves = []
 
-    def setxy(self, x, y):
-        self.x = x
-        self.y = y
+    def set_pos(self, row, col):
+        self.row = row
+        self.col = col
+
+    def move(self,floor_grid):
+        self.open_moves = []
+        moves = [-1,1]
+        for row in moves:
+            for col in moves:
+                if not floor_grid[self.row + row][self.col + col]:
+                    self.open_moves.append([self.row + row,self.col +col])
+        next_move = self.open_moves[random.randint(range(len(self.open_moves)))]
+        self.set_pos(next_move[0], next_move[1])
+
+
+
+
+
 
     def update(self, player):
         pass
+
+
+
