9c0ed2800229e0dd8cd9886bd545a00c744944f7
csjoey <Joelopor@udel.edu>
2019-11-26 16:45:51 -0500

    Added save loading and saving and integrated into engine

--- engine/classPlayerData.py
+++ engine/classPlayerData.py
@@ -1,7 +1,44 @@
-import os
-class PlayerData():
+import json
+class PlayerData:
     """
     Class for the storage and retrieval of player saves
     """
     def __init__(self):
-        pass
+        self.max_health_upgrades = None
+        self.max_speed_upgrades = None
+        self.coins = None
+        self.playtime = None
+
+    def load_local_save(self):
+
+        try:
+            with open("savegame/data_file.json", "r") as read_file:
+                data = json.load(read_file)
+                self.max_health_upgrades = data['save_game']["max_health_upgrades"]
+                self.max_speed_upgrades = data['save_game']["max_speed_upgrades"]
+                self.coins = data['save_game']["coins"]
+                self.playtime = data['save_game']["playtime"]
+        except:
+            self.blank_save()
+            self.save_local_save()
+            self.load_local_save()
+
+
+    def save_local_save(self):
+        self.blank_save()
+        data = {
+            "save_game": {
+                "max_health_upgrades": self.max_health_upgrades,
+                "max_speed_upgrades": self.max_speed_upgrades,
+                "coins": self.coins,
+                "playtime": self.playtime
+            }
+        }
+        with open("savegame/data_file.json", "w") as write_file:
+            json.dump(data, write_file)
+
+    def blank_save(self):
+        self.max_health_upgrades = 0
+        self.max_speed_upgrades = 0
+        self.coins = 0
+        self.playtime = 0
